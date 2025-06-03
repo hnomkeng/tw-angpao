0008e3d0916b29a96b61f435690b48e01438d17b
pichxyaponn <pichxyaponn@gmail.com>
2025-06-04 03:27:57 +0700

    🎉 (release): 1.9.2

--- example/index.ts
+++ example/index.ts
@@ -1,41 +1,38 @@
 // example/index.ts
 
-import { Elysia, t } from 'elysia'
-import { TWAngpao } from '../src'
+import { Elysia, t } from "elysia";
+import { TWAngpao } from "../src";
 
 const app = new Elysia()
-	.use(TWAngpao('TWA'))
-	.post(
-		'/redeem',
-		async ({ body, TWA }) => {
-			const response = await TWA.redeem(
-				body.phoneNumber,
-				body.voucherCode
-			)
-			if (response.status.code !== 'SUCCESS') {
-				// If not success
-				return {
-					status: {
-						code: response.status.code,
-						message: response.status.message
-					}
-				}
-			}
+  .use(TWAngpao("TWA"))
+  .post(
+    "/redeem",
+    async ({ body, TWA }) => {
+      const response = await TWA.redeem(body.phoneNumber, body.voucherCode);
+      if (response.status.code !== "SUCCESS") {
+        // If not success
+        return {
+          status: {
+            code: response.status.code,
+            message: response.status.message
+          }
+        };
+      }
 
-			return {
-				// If successful
-				status: {
-					code: 'SUCCESS',
-					message: 'Voucher redeemed successfully!'
-				},
-				data: response.data
-			}
-		},
-		{
-			body: t.Object({
-				phoneNumber: t.String(),
-				voucherCode: t.String()
-			})
-		}
-	)
-	.listen(3000)
+      return {
+        // If successful
+        status: {
+          code: "SUCCESS",
+          message: "Voucher redeemed successfully!"
+        },
+        data: response.data
+      };
+    },
+    {
+      body: t.Object({
+        phoneNumber: t.String(),
+        voucherCode: t.String()
+      })
+    }
+  )
+  .listen(3000);
